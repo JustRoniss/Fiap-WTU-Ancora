d6ca4179346df82b309b3fc3e7ddfd300e5f7a58
justroniss <rm96679@fiap.com.br>
2024-10-21 19:48:39 -0300

    Ajustando UnitService/Controller - padronizando retorno da API

--- src/main/java/fiap/wtu_ancora/controller/UnitController.java
+++ src/main/java/fiap/wtu_ancora/controller/UnitController.java
@@ -1,9 +1,13 @@
 package fiap.wtu_ancora.controller;
 
+import fiap.wtu_ancora.domain.Unit;
 import fiap.wtu_ancora.dto.UnitDTO;
+import fiap.wtu_ancora.model.ApiReponse;
 import fiap.wtu_ancora.service.UnitService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/units")
@@ -16,22 +20,25 @@ public class UnitController {
     }
 
     @GetMapping("/get-all")
-    public ResponseEntity<?> getAllUnits() {
+    public ResponseEntity<ApiReponse<List<Unit>>> getAllUnits() {
+
         return unitService.getAllUnits();
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createUnit(@RequestBody UnitDTO unitDTO) {
+    public ResponseEntity<ApiReponse<String>> createUnit(@RequestBody UnitDTO unitDTO) {
+
         return unitService.createUnit(unitDTO);
     }
 
     @PutMapping("/edit/{id}")
-    public ResponseEntity<?> editUnit(@PathVariable Long id, @RequestBody UnitDTO unitDTO) {
+    public ResponseEntity<ApiReponse<String>> editUnit(@PathVariable Long id, @RequestBody UnitDTO unitDTO) {
         return unitService.updateUnit(id, unitDTO);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteUnit(@PathVariable Long id) {
+    public ResponseEntity<ApiReponse<String>> deleteUnit(@PathVariable Long id) {
+
         return unitService.deleteUnit(id);
     }
 }
